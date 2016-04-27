ff6b4a9ce1922ef51449fc735f9829122e416c9a
dengri12 <dengri12@gmail.com>
2016-04-27 11:08:39 +0300

    Adds router to app.js and renames *.html to *.htm

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 //MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+weatherApp.config(function($routeProvider){
+	$routeProvider
+
+		.when('/', {
+			templateUrl: 'pages/home.htm',
+			controller: 'homeController'
+		})
+
+
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.htm',
+			controller: 'forecastController'
+		})
+});
+
+
 weatherApp.controller('homeController', ['$scope', function($scope){
 	$scope.test = 'test 1';
 	console.log($scope.test);
