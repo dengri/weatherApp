73229fe463f1ad942078cbf24b3d829e7d26d4cb
dengri12 <dengri12@gmail.com>
2016-04-27 10:32:16 +0300

    Adds two controllers: homeController, forecatController

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.controller('homeController', ['$scope', function($scope){
+	$scope.test = 'test 1';
+	console.log($scope.test);
+}]);
+
+
+weatherApp.controller('forecastController', ['$scope', function($scope){
+	$scope.test = 'test 2';
+	console.log($scope.test);
+}]);
